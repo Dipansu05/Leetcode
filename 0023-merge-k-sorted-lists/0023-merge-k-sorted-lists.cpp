189f7601dca4a33c77a1c4d9267fc4f7bd7f7086
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-03-12 16:07:10 +0530

    Time: 32 ms (40.85%), Space: 13.2 MB (74.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,41 +1,39 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class cmp{
+    public:
+    bool operator() (ListNode* a, ListNode* b){
+        return a->val>b->val;
+        }
+};
 class Solution {
 public:
-  ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
-        return help(lists,0,lists.size()-1);
-    }
-    ListNode* help(vector<ListNode*> v,int s,int e){
-        if(s==e) return v[s];
-        int mid=(s+e)/2;
-        ListNode* l= help(v,s,mid);
-        ListNode* r=help(v,mid+1,e);
-        return merge(l,r);
-        }
-    ListNode* merge(ListNode* l,ListNode* r){
-        ListNode* ans=new ListNode(-1);
-        ListNode* temp=ans;
-        while(l!=NULL && r!=NULL){
-            if(l->val<r->val){
-                temp->next=l;
-                l=l->next;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,cmp> q; 
+        ListNode* dummy=new ListNode(-1);
+        ListNode* tail=dummy;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]!=NULL){
+                q.push(lists[i]);
             }
-            else{
-                temp->next=r;
-                r=r->next;
-                }
-            temp=temp->next;
         }
-        if(l!=NULL){
-            temp->next=l;
-            l=l->next;
-            temp=temp->next;
-        }
-        if(r!=NULL){
-            temp->next=r;
-            r=r->next;
-            temp=temp->next;
-        }
-        return ans->next;
+            while(q.size()){
+                ListNode* temp=q.top();
+                tail->next=temp;
+                tail=temp;
+                q.pop();
+                if(temp->next!=NULL)q.push(temp->next);
+            }
+        
+        
+        return dummy->next;
     }
 };
