99dc6ee9af8991466057e473e9d3e0fa6b089240
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-03-12 16:05:25 +0530

    Time: 312 ms (11.75%), Space: 350.5 MB (5.28%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,15 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        return help(lists,0,lists.size()-1);
+    }
+    ListNode* help(vector<ListNode*> v,int s,int e){
+        if(s==e) return v[s];
+        int mid=(s+e)/2;
+        ListNode* l= help(v,s,mid);
+        ListNode* r=help(v,mid+1,e);
+        return merge(l,r);
+        }
     ListNode* merge(ListNode* l,ListNode* r){
         ListNode* ans=new ListNode(-1);
         ListNode* temp=ans;
@@ -21,8 +23,7 @@ public:
             else{
                 temp->next=r;
                 r=r->next;
-                
-            }
+                }
             temp=temp->next;
         }
         if(l!=NULL){
@@ -36,18 +37,5 @@ public:
             temp=temp->next;
         }
         return ans->next;
-    }
-    ListNode* help(vector<ListNode*> v,int s,int e){
-        if(s==e) return v[s];
-        int mid=(s+e)/2;
-       ListNode* l= help(v,s,mid);
-        ListNode* r=help(v,mid+1,e);
-       return
-           merge(l,r);
-        
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
-        return help(lists,0,lists.size()-1);
     }
 };
