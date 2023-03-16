24186fcf9cc9e967cc6d421442fd29072410b9c8
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-03-16 18:29:10 +0530

    Time: 20 ms (65.05%), Space: 26.3 MB (67.57%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -13,11 +13,9 @@ class Solution {
 public:
     TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& postorder, unordered_map<int, int>& ump, int& rootIdx, int left, int right) {
         if(left > right) return NULL;
-        
         int pivot = ump[postorder[rootIdx]];
         TreeNode* node = new TreeNode(postorder[rootIdx]);
         rootIdx--;
-
         node->right = buildTreeHelper(inorder, postorder, ump, rootIdx, pivot+1, right);
         node->left = buildTreeHelper(inorder, postorder, ump, rootIdx, left, pivot-1);
         return node;
