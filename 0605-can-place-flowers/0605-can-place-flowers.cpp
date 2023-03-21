ca4355dc7f56da7ee0fcfec3bdeff9a158866622
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-03-21 20:54:40 +0530

    Time: 26 ms (17.96%), Space: 20.3 MB (34.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& f, int n) {
-       int c =0;
+     
         for(int i=0;i<f.size();i++){
             if(f[i]==0 and (i==0 || f[i-1]==0) and (i==f.size()-1 || f[i+1]==0)){
                 f[i]=1;
-                c++;
+                n--;
                 }
         }
-        if(c < n)  return false;
+        if(n>0)  return false;
         return true;
     }
 };
