07215631745fd34b51b1e753f20398474affc5c2
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-03-27 16:33:25 +0530

    Time: 7 ms (90.29%), Space: 9.7 MB (82.48%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -5,15 +5,9 @@ public:
         int m=grid[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 and j!=0){
-                    grid[i][j]+=grid[i][j-1];
-                }
-                if(i!=0 and j==0){
-                    grid[i][j]+=grid[i-1][j];
-                }
-                if(i!=0 and j!=0){
-                    grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
-                }
+                if(i==0 and j!=0) grid[i][j]+=grid[i][j-1];
+                if(i!=0 and j==0) grid[i][j]+=grid[i-1][j];
+                if(i!=0 and j!=0) grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
             }
         }
         return grid[n-1][m-1];
