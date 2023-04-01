672880f8a3b2e7e7cbb2f965f5c3dca993f8c970
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-04-01 09:37:03 +0530

    Time: 45 ms (27.23%), Space: 27.5 MB (69.64%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& n, int t) {
-        int l=0;
-        int r=n.size()-1;
-        int m;
-        while(l<=r){
-            m=(l+r)/2;
-            if(n[m]==t) return m;
-            else if(n[m]<t) l=m+1;
-            else r=m-1;
+        int l = 0, r = n.size()-1, m;
+        while(l <= r){
+            m = ( l + r) /2;
+            if ( n[m] == t) return m;
+            else if(n[m] < t) l = m+1;
+            else r = m-1;
         }
         return -1;
     }
