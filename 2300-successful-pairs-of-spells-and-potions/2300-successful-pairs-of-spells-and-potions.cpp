dfb52e4ce1af5608a1dd32cf34c52fe3d0ce05cc
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-04-02 16:59:07 +0530

    Time: 249 ms (64.68%), Space: 100 MB (31.79%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
-    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        vector<int> ans;
-        sort(potions.begin(),potions.end());
-        for(int x:spells){
-            long long need=(success+x-1)/x;
-            auto it=lower_bound(potions.begin(),potions.end(),need);
-            ans.push_back(potions.end()-it);
+    int solve(vector<int>& potions, long long success, long long a, int m){
+        int s = 0;
+        int e = m-1;
+        int mid = s + (e-s)/2;
+        int ans = -1;
+        
+        while(s <= e){
+            long long b = potions[mid];
+            if(a*b >= success){
+                e = mid - 1;
+                ans = mid;
+            }
+            else{
+                s = mid+1;
+            }
+            mid = s + (e-s)/2;
         }
         return ans;
+    }
+    
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        sort(potions.begin(), potions.end());
+        vector<int> ans;
+        for(int i=0; i<n; i++){
+            long long a = spells[i];
+            if(a >= success){
+                ans.push_back(m);
+                continue;
+            }
+            int index = solve(potions, success, a, m);
+            if(index == -1) ans.push_back(0);
+            else  ans.push_back(m - index);
+        }
+         return ans;
     }
 };
