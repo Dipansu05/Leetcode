e49d2673714b9f8963ddb25972baa3ec28486f04
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-04-28 10:32:31 +0530

    Time: 1194 ms (5.07%), Space: 12.9 MB (43.81%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int numSimilarGroups(vector<string>& strs) {
-        
-     unordered_set<string>st;
+      unordered_set<string>st;
       for(auto s:strs)
-        st.insert(s);
-     
+      st.insert(s);
       int count=0;
       for(int i=0;i<strs.size();i++){
         if(st.find(strs[i])!=st.end()){
@@ -15,8 +13,7 @@ public:
       }
       return count;
     }
-  
-     void dfs(string s,vector<string>&strs,unordered_set<string>&st){
+void dfs(string s,vector<string>&strs,unordered_set<string>&st){
        st.erase(s);
        for(int i=0;i<s.length()-1;i++){
          for(int j=i+1;j<s.length();j++){
