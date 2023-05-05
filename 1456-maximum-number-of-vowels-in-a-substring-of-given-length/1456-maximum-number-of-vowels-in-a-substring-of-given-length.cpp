0f71293f36b207f375972784a2442edda75bec78
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-05-05 13:25:49 +0530

    Time: 26 ms (40.98%), Space: 10.1 MB (29.12%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
+     int i=0;
         int count = 0;
-        int temp = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                temp++;
+        int ans = 0;
+        
+        for(int j= 0 ; j < s.size() ; j++){
+                
+            if(s[j] == 'a') count++;
+            if(s[j] == 'e') count++;
+            if(s[j] == 'i') count++;
+            if(s[j] == 'o') count++;  
+            if(s[j] == 'u') count++;
+            
+            while(j-i+1 > k){          
+                
+                if(s[i] == 'a') count--;
+                if(s[i] == 'e') count--;
+                if(s[i] == 'i') count--;
+                if(s[i] == 'o') count--;  
+                if(s[i] == 'u') count--;
+                
+                i++;
+              
             }
-            if (i - k >= 0){
-                if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u'){
-                    temp--;
-                }
-            }
-            count = max(count, temp);
+            
+            ans = max(ans,count);    
         }
-        return count;
+        
+        return ans;
     }
 };
