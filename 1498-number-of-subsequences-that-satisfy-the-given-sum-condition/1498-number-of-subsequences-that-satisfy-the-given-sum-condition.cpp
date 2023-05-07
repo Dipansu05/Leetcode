8c74542f25d043a13159727c5e478da5bebd22f1
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-05-08 00:07:39 +0530

    Time: 200 ms (10.90%), Space: 47.6 MB (99.60%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,24 +1,25 @@
+long modVal = 1000000007;
+
 class Solution {
-public:
-     int MOD = 1e9 + 7;
-    int numSubseq(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int l = 0, r = n - 1;
-        long long res = 0;
-        vector<int>power(n);
-        power[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            power[i] = (power[i - 1] * 2) % MOD;
+    long powMod(int base, int power) {
+        long res = 1, multi = base;
+        while (power) {
+            if (power & 1) res *= multi, res %= modVal;
+            multi *= multi, multi %= modVal;
+            power >>= 1;
         }
-        while (l <= r) {
-            if (nums[l] + nums[r] > target) {
-                r--;
-            } else {
-                int len = r - l;
-                res = (res + power[len]) % MOD;
-                l++;
-            }
+        return res;
+    }
+public:
+    int numSubseq(vector<int> &nums, int target) {
+        long res = 0;
+        sort(begin(nums), end(nums));
+        for (int i = 0, j, len = nums.size(), n, m; i < len; i++) {
+            n = nums[i], m = target - n;
+            j = upper_bound(begin(nums) + i, end(nums), m) - begin(nums);
+            if (j == i) break;
+            else res += powMod(2, j - i - 1);
+            if (res > modVal) res -= modVal;
         }
         return res;
     }
