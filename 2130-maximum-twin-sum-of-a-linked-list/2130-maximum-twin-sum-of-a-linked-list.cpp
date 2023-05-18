db253596a90c688abd79d7e8fcb2042b5100b94a
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-05-18 11:22:56 +0530

    Time: 272 ms (49.14%), Space: 126.8 MB (9.65%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,38 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) 
-    {
-        ListNode* prev=NULL;
-        ListNode* cur=head;
-        ListNode* next;
-        
-        while(cur){
-            next=cur->next;
-            cur->next=prev;
-            prev=cur;
-            cur=next;
-        }
-        return prev;
-    }
     int pairSum(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-       
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        slow=reverseList(slow);
+        vector<int> temp;
         int ans=INT_MIN;
-        
-        while(slow!=NULL){
-            ans=max(ans,slow->val+head->val);
-            slow=slow->next;
-            head=head->next;    
+        ListNode* t=head;
+        while(t!=NULL){
+            temp.push_back(t->val);
+            t=t->next;
+        }
+        int i=0,j=temp.size()-1;
+        while(i<j){
+            if(temp[i]+temp[j]>ans) ans=temp[i]+temp[j];
+            i++;
+            j--;
         }
         return ans;
-        
-		
-        
     }
 };
