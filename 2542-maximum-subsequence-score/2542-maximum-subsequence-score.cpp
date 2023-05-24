9f7c50d5f65aabd58d20b9ff5b2ac0ba72d6b48b
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-05-24 10:51:55 +0530

    Time: 264 ms (77.34%), Space: 91.9 MB (72.17%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int, int>> p ;
-        
-        for(int i = 0 ; i<nums1.size() ; i++){
-            p.push_back({nums2[i] , nums1[i]});
-        }
-        
-        sort(p.rbegin() , p.rend());
-        
-        long long ans  = 0;
-        long long sum  = 0;
-
-        priority_queue<int> pq;
-        for(int i = 0 ; i<k-1 ; i++){
-            sum += p[i].second;
-            pq.push(-p[i].second);
-        }
-
-        for(int i = k-1 ; i<nums1.size() ; i++){
-            sum += p[i].second;
-            pq.push(-p[i].second);
+vector<pair<int,int>> p;
+        for(int i=0;i<nums1.size();i++){
+            p.push_back({nums2[i],nums1[i]});
             
-            ans = max(ans, sum * p[i].first );
-
-            sum += pq.top();
-            pq.pop();
         }
-        
-        return ans;  
+        sort(p.begin(),p.end());
+        long long ans=0;
+        long long sum=0;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=nums1.size()-1;i>=0;i--){
+            sum+=p[i].second;
+            pq.push(p[i].second);
+            if(pq.size()>k){
+                sum=sum-pq.top();
+                pq.pop();
+            }
+            if(pq.size()==k){
+                ans=max(ans,sum*p[i].first);
+            }
+        }
+        return ans;
+            
     }
-            
-    
 };
