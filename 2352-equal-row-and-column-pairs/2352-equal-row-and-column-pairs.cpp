e2371ee4f63da67acecd5bcdb6498eb7eb64abef
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-14 16:38:12 +0530

    Time: 123 ms (51.41%), Space: 38.2 MB (33.66%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        map<vector<int>,int >m;
-       int n = grid.size();
-       for(int i =0;i<n;i++){
-           vector<int>temp;
-           for(int j =0;j<n; j++){
-            temp.push_back(grid[i][j]);
-           }
-           m[temp]++;
-       }
-      
-       int cnt = 0;
-        for(int j =0;j<n;j++){
-          vector<int>temp;
-           for(int i =0;i<n; i++){
-               temp.push_back(grid[i][j]);
-           }
-           if(m.find(temp) != m.end()){
-               cnt += m[temp];
-           }
-           }
-           return cnt;
+        map<vector<int>,int> m;
+        int n=grid.size();
+        for(int i=0;i<n;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                temp.push_back(grid[i][j]);
+            }
+            m[temp]++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                temp.push_back(grid[j][i]);
+            }
+            if(m.find(temp)!=m.end()){
+                ans+=m[temp];
+            }
+        }
+        return ans;
     }
 };
