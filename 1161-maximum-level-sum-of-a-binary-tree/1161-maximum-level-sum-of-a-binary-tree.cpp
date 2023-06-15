345a62707a794011b7f5cc07d94555eab7a89e02
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-15 16:44:42 +0530

    Time: 216 ms (29.67%), Space: 107.3 MB (24.55%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,28 +11,27 @@
  */
 class Solution {
 public:
-    void levelor(vector<vector<int>> &ans, TreeNode* root, int level){
-        if(root==NULL) return;
-        if(level==ans.size()) ans.push_back({root->val});
-        else
-            ans[level].push_back(root->val);
-            levelor(ans,root->left,level+1);
-            levelor(ans,root->right,level+1);
-    }
     int maxLevelSum(TreeNode* root) {
-        vector<vector<int>> ans;
-        levelor(ans,root,0);
-        int a=INT_MIN,d=0;
-        for(int i=0;i<ans.size();i++){
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxs=INT_MIN;
+        int l=1,ans;
+        while(!q.empty()){
+            int n=q.size();
             int t=0;
-            for(int j=0;j<ans[i].size();j++){
-                t+=ans[i][j];
+            while(n--){
+                TreeNode* tm=q.front();
+                q.pop();
+                t+=tm->val;
+                if(tm->left) q.push(tm->left);
+                if(tm->right) q.push(tm->right);
             }
-            if(t>a){
-                a=t;
-                d=i+1;
+            if(t>maxs){
+                maxs=t;
+                ans=l;
             }
+            l++;
         }
-        return d;
+        return ans;
     }
 };
