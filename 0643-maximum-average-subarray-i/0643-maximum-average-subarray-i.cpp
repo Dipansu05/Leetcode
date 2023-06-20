4cedfc7f3276621ae1f01f6bd301bf8aa8630669
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-20 18:22:02 +0530

    Time: 193 ms (84.94%), Space: 109.6 MB (75.21%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -3,13 +3,14 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         double ans=INT_MIN;
         double sum=0;
+        int n=nums.size();
         for(int i=0;i<k;i++){
             sum+=nums[i];
         }
         ans=sum/k;
-        for(int i=k;i<nums.size();i++){
+        for(int i=k;i<n;i++){
             sum+=nums[i];
-                sum-=nums[i-k];
+            sum-=nums[i-k];
             ans=max(ans,sum/k);
         }
         return ans;
