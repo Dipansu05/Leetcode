8eada6df6cb99cdf28a4cb24a3378127edff49f5
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-22 17:28:03 +0530

    Time: 21 ms (30.98%), Space: 7.1 MB (12.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-       int l =0 ; 
-        int mx=0;
-        int res=0;
-        map<char,int>mp ;
-        
-        
-        for(int i =0 ;i < s.size() ; i++)
-        {
-            mp[s[i]]++;
-            
-            mx=max(mx,mp[s[i]]);
-            
-            if(i-l+1-mx>k)
-            {
-                mp[s[l]]--;
+      map<char,int>  m;
+        int ans=0,l=0,mx=0,n=s.size();
+        if(n==0) return 0;
+        for(int i=0;i<n;i++){
+            m[s[i]]++;
+            mx=max(mx,m[s[i]]);
+            if(i-l+1-mx>k){
+                m[s[l]]--;
                 l++;
             }
-            res=max(res,i-l+1);
-            
+            ans=max(i-l+1,ans);
         }
-        
-        return res;
+        return ans;
     }
 };
