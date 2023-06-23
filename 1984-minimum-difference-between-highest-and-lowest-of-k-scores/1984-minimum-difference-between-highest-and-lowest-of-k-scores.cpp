6599d422e4d3cef9105b1eab0228a1514121f44a
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-23 18:18:39 +0530

    Time: 19 ms (62.56%), Space: 13.6 MB (92.96%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -3,8 +3,8 @@ public:
     int minimumDifference(vector<int>& nums, int k) {
      sort(nums.begin(),nums.end());
         k--;
-        int ans=INT_MAX;
-        for(int i=0;i<nums.size()-k;i++){
+        int ans=INT_MAX,n=nums.size()-k;
+        for(int i=0;i<n;i++){
         if(nums[i+k]-nums[i]<ans) ans=nums[i+k]-nums[i];
         }
         return ans;
