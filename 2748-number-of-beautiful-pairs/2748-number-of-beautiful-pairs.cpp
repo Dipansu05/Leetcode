715bec17b4f450da6d5379d2f73dfd344c55743a
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2023-06-25 11:58:02 +0530

    Time: 255 ms (37.50%), Space: 67.6 MB (87.50%) - LeetHub

---
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countBeautifulPairs(vector<int>& nums) {
+        int ans=0,n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(__gcd(to_string(nums[i])[0]-'0',nums[j]%10)==1) ans++;
+            }
+        }
+        return ans;
+    }
+};
