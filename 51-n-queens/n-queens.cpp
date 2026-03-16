1e35af9a029ecb7333ca9bb71f6a85102c965221
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-16 19:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (75.70%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     bool isSafe(int row, int col, vector<string>& board, int n){
+
         int dupRow=row;
         int dupCol=col;
 
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+        }
+
+        row=dupRow;
+        col=dupCol;
         while(row>=0 && col>=0){
             if(board[row][col]=='Q') return false;
             row--;
             col--;
         }
-        col=dupCol;
-        row=dupRow;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
+
         row=dupRow;
         col=dupCol;
         while(row<n && col>=0){
@@ -22,29 +25,34 @@ public:
             row++;
             col--;
         }
+
         return true;
     }
-    void solve(int col, vector<vector<string>> &ans, vector<string>& board, int n){
+    void solve(int col, vector<vector<string>>& ans, vector<string>& board, int n){
+
         if(col==n){
             ans.push_back(board);
             return;
         }
+
         for(int row=0;row<n;row++){
-            if(isSafe(row, col, board,n)){
+            if(isSafe(row, col, board, n)){
                 board[row][col]='Q';
                 solve(col+1,ans,board,n);
                 board[row][col]='.';
             }
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
 
         vector<vector<string>> ans;
-        string s(n,'.');
         vector<string> board(n);
+        string s(n,'.');
+
         for(int i=0;i<n;i++) board[i]=s;
 
-        solve(0,ans,board,n);
+        solve(0, ans, board, n);
         return ans;
     }
 };
