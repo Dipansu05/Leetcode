bf64b9b4157ea1981cabfe9c598db6b45153c049
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-16 18:59:48 +0530

    Time: 4 ms (31.67%) | Memory: 10.1 MB (75.70%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> board, int n){
-        int duprow=row;
-        int dupcol=col;
+    bool isSafe(int row, int col, vector<string>& board, int n){
+        int dupRow=row;
+        int dupCol=col;
 
         while(row>=0 && col>=0){
             if(board[row][col]=='Q') return false;
             row--;
             col--;
         }
-
-        col=dupcol;
-        row=duprow;
+        col=dupCol;
+        row=dupRow;
         while(col>=0){
             if(board[row][col]=='Q') return false;
             col--;
         }
-
-        row=duprow;
-        col=dupcol;
+        row=dupRow;
+        col=dupCol;
         while(row<n && col>=0){
             if(board[row][col]=='Q') return false;
             row++;
@@ -26,26 +24,27 @@ public:
         }
         return true;
     }
-    void solve(int col, vector<string>&board, vector<vector<string>>& ans, int n){
+    void solve(int col, vector<vector<string>> &ans, vector<string>& board, int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isSafe(row, col, board, n)){
+            if(isSafe(row, col, board,n)){
                 board[row][col]='Q';
-                solve(col+1,board, ans, n);
+                solve(col+1,ans,board,n);
                 board[row][col]='.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0; i<n; i++) board[i]=s;
 
-        solve(0,board, ans, n);
+        vector<vector<string>> ans;
+        string s(n,'.');
+        vector<string> board(n);
+        for(int i=0;i<n;i++) board[i]=s;
+
+        solve(0,ans,board,n);
         return ans;
     }
 };
