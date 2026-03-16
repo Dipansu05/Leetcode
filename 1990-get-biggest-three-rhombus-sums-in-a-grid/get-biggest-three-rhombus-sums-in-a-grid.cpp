2f68d400fa50ac36e59bf8ca57639ad6be1bc9a0
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-16 13:44:03 +0530

    Time: 208 ms (26.20%) | Memory: 61.9 MB (36.55%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,57 +1,44 @@
-    unsigned diag[100][51], antid[100][51];
-    const int OFFSET=50;
-
 class Solution {
 public:
-
-    static inline int rhombusSum(int i, int j, int d, vector<vector<int>>& grid){
-        if(d==0) return grid[i][j];
-        const int l=j-d, r=j+d, u=i-d, b=i+d;
-
-        const int i0=u-j+OFFSET, i1=i-l+OFFSET;
-        int sum=diag[i0][r+1]-diag[i0][j];
-        sum+=diag[i1][j+1]-diag[i1][l];
-
-        const int j0=u+j,j1=b+j;
-        sum+=antid[j0][i]-antid[j0][u+1];
-        sum+=antid[j1][b]-antid[j1][i+1];
-
-        return sum;
-    }
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
-        const int m=grid.size(), n=grid[0].size();
+        int m=grid.size();
+        int n=grid[0].size();
+        set<int> st;
 
-        for(int i=0; i<m; i++){
+        for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                const int i0=i-j+OFFSET, j0=i+j;
-                const int x=grid[i][j];
+                st.insert(grid[i][j]); 
 
-                diag[i0][j+1]=diag[i0][j]+x;
-                antid[j0][i+1]=antid[j0][i]+x;
-            }
-        }
+                for(int k=1;; k++){
+                    if(i-k<0 || i+k>=m || j-k<0 || j+k>=n) break;
 
-        int dM=min(m,n)/2;
-        vector<int> x(3,-1);
-        for(int d=0; d<=m; d++){
-            for(int i=d;i<m-d;i++){
-                for(int j=d;j<n-d;j++){
-                    const int y=rhombusSum(i,j,d,grid);
+                    int sum =0;
+                    int r=i-k;
+                    int c=j;
+                    for(int t=0;t<k;t++) sum+=grid[r+t][c+t];
 
-                    if(y==x[0] || y==x[1] || y==x[2]) continue;
+                    r=i;
+                    c=j+k;
+                    for(int t=0;t<k;t++) sum+=grid[r+t][c-t];
 
-                    if(y>x[0]){
-                        x[2]=x[1]; x[1]=exchange(x[0],y);
-                    }
-                    else if(y>x[1]) x[2]=exchange(x[1], y);
-                    else if(y>x[2]) x[2]=y;
+                    r=i+k;
+                    c=j;
+                    for(int t=0;t<k;t++) sum+= grid[r-t][c-t];
+
+                    r=i;
+                    c=j-k;
+                    for(int t=0;t<k;t++) sum+= grid[r-t][c+t];
+
+                    st.insert(sum);
+
                 }
             }
         }
-        for(int i=2; i>=0; i--){
-            if(x[i]==-1) x.pop_back();
-        }
-            return x;
-        
+        vector<int> ans;
+        auto it = st.rbegin();
+
+        for(int i=0;i<3 && it!=st.rend();i++,it++) ans.push_back(*it);
+
+        return ans;
     }
 };
