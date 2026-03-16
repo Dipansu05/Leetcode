b94b55f2c335ac21bbd83080f269718b36d210f3
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-16 14:26:57 +0530

    Time: 210 ms (23.45%) | Memory: 61.7 MB (47.59%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        set<int> st;
+        int n=grid.size();
+        int m=grid[0].size();
+         set<int> test;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                st.insert(grid[i][j]); 
+        for(int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
 
-                for(int k=1;; k++){
-                    if(i-k<0 || i+k>=m || j-k<0 || j+k>=n) break;
+               
+                test.insert(grid[i][j]);
 
-                    int sum =0;
+                for(int k=1;;k++){
+                    if(i-k<0 || j-k<0 || i+k >=n || j+k>=m) break;
+
+                    int sum{0};
                     int r=i-k;
                     int c=j;
                     for(int t=0;t<k;t++) sum+=grid[r+t][c+t];
@@ -23,22 +25,24 @@ public:
 
                     r=i+k;
                     c=j;
-                    for(int t=0;t<k;t++) sum+= grid[r-t][c-t];
+                    for(int t=0;t<k;t++) sum+=grid[r-t][c-t];
 
                     r=i;
                     c=j-k;
-                    for(int t=0;t<k;t++) sum+= grid[r-t][c+t];
+                    for(int t=0;t<k;t++) sum+=grid[r-t][c+t];
 
-                    st.insert(sum);
+                    test.insert(sum);
 
                 }
             }
         }
-        vector<int> ans;
-        auto it = st.rbegin();
 
-        for(int i=0;i<3 && it!=st.rend();i++,it++) ans.push_back(*it);
+        vector<int> ans;
+
+        auto it=test.rbegin();
+        for(int i=0;i<3 && it!=test.rend();i++,it++) ans.push_back(*it);
 
         return ans;
+        
     }
 };
