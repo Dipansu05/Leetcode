b813ed6b620b4a86c8f9ac4145aaa68c45f451f9
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-17 18:57:17 +0530

    Time: 65 ms (20.21%) | Memory: 93.6 MB (26.06%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int maxArea=0;
-        vector<int> h(n,0);
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int ans{0};
+        vector<int> h(col,0);
 
-        for(int i=0; i<m; i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(matrix[i][j]==1) h[j]++;
-                else h[j] =0;
+                else h[j]=0;
             }
-            vector<int> sh=h;
+            vector<int> sh = h;
             sort(sh.rbegin(), sh.rend());
-            for(int j=0;j<n;j++){
+            for(int j=0;j<col;j++){
                 if(sh[j]==0) break;
-                maxArea =max(maxArea, sh[j]*(j+1));
+                ans=max(ans,(j+1)*sh[j]);
             }
         }
-        return maxArea;
+
+        return ans;
     }
 };
