02999fd0041b46a0f99a812cf22e78646802b144
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-18 10:28:42 +0530

    Time: 12 ms (74.72%) | Memory: 90.6 MB (60.67%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int n=grid.size(), m=grid[0].size();
-        int ans{0};
-        vector<vector<int>>prefixSum(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                prefixSum[i][j]=grid[i][j];
-                if(i>0)prefixSum[i][j]+=prefixSum[i-1][j];
-                if(j>0)prefixSum[i][j]+=prefixSum[i][j-1];
-                if(i>0 && j>0)prefixSum[i][j]-=prefixSum[i-1][j-1];
-                if(prefixSum[i][j]<=k)ans++;
+        int m=grid.size();
+        int n=grid[0].size();
+        int ans=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up   = (i>0 ? grid[i-1][j] : 0);
+                int left = (j>0 ? grid[i][j-1] : 0);
+                int diag = (i>0 && j>0 ? grid[i-1][j-1] : 0);
+
+                grid[i][j] = grid[i][j] + up + left - diag;
+
+                if(grid[i][j] <= k) ans++;
+                else{
+                    if(j==0) return ans;
+                    break;
+                }
             }
         }
+
         return ans;
+
     }
 };
