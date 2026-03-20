f86e5fea343faa12af761de84a8710637cb8cb39
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-20 18:57:48 +0530

    Time: 314 ms (60.60%) | Memory: 8.7 MB (93.43%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& board, int row, int col, char c){
-
-        for(int i=0;i<9  ;i++){
-            if(board[i][col]==c) return false;
-            if(board[row][i]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
-        }
-
-        return true;
-
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
     }
     bool solve(vector<vector<char>>& board){
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(board[i][j]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isValid(board,i,j,c)){
-                        board[i][j]=c;
-                        if(solve(board)==true) return true;
-                        else board[i][j]='.';
+        for(int row=0;row< 9;row++){
+            for(int col=0;col<9;col++){
+                if(board[row][col]=='.'){
+                    for(char num='1';num<='9';num++){
+                    if(isValid(board, row, col, num)){
+                        board[row][col]=num;
+                        if(solve(board)) return true;
+                        board[row][col]='.';
                     }
                 }
                 return false;
+                }
             }
-        }
         }
         return true;
     }
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num){
+        for(int i=0; i<9; i++){
+            if(board[row][i]==num || board[i][col]==num) return false;
+        }
+        int startRow = (row/3) * 3, startCol = (col/3)* 3;
+        for(int i= startRow; i < startRow+3; i++){
+            for(int j=startCol; j< startCol+3; j++){
+                if(board[i][j]==num) return false;
+            }
+        }
+        return true;
     }
 };
