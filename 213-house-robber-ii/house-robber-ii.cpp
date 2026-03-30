8ec1e6b29824fd0a5ad3c5e96099d1acdc633fdb
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-03-30 12:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (7.41%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -17,16 +17,14 @@ public:
         return max(include[n-1], exclude[n-1]);
     }
     int rob(vector<int>& nums) {
-        vector<int> temp1;
+        vector<int> temp1, temp2;
         int n=nums.size();
         if(n==1) return nums[0];
-        for(int i=1;i<n;i++) temp1.push_back(nums[i]);
-        int t1=help(temp1);
+        for(int i=0;i<n;i++){
+            if(i!=0)   temp1.push_back(nums[i]);
+            if(i!=n-1) temp2.push_back(nums[i]);
+        }
 
-        vector<int> temp2;
-        for(int i=0;i<n-1;i++) temp2.push_back(nums[i]);
-        int t2=help(temp2);
-
-        return max(t1,t2);
+     return max(help(temp1), help(temp2));
     }
 };
