b7c53e9c9d0a78c43e7d95067c4edd70e421c0be
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-07 17:21:24 +0530

    Time: 27 ms (69.83%) | Memory: 128 MB (43.97%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -1,15 +1,12 @@
 class Robot {
 public:
-
     bool moved=false;
     int idx=0;
-    vector<pair<int, int>> pos;
     vector<int> dir;
-
+    vector<pair<int, int>> pos;
     unordered_map<int, string> to_dir={
-        {0,"East"},{1,"North"},{2,"West"},{3,"South"}
-        };
-
+        {0,"East"}, {1,"North"}, {2,"West"}, {3,"South"}
+    };
     Robot(int width, int height) {
         for(int i=0;i<width;i++){
             pos.emplace_back(i,0);
@@ -20,7 +17,7 @@ public:
             dir.emplace_back(1);
         }
         for(int i=width-2;i>=0;i--){
-            pos.emplace_back(i, height-1);
+            pos.emplace_back(i,height-1);
             dir.emplace_back(2);
         }
         for(int i=height-2;i>0;i--){
@@ -28,23 +25,21 @@ public:
             dir.emplace_back(3);
         }
         dir[0]=3;
-
-        
     }
     
     void step(int num) {
         moved=true;
-        idx=(idx+num) % pos.size();
+        idx=(idx+num)%pos.size();
     }
     
     vector<int> getPos() {
-        return {pos[idx].first, pos[idx].second};
+        return {pos[idx].first,pos[idx].second};
     }
     
     string getDir() {
         if(!moved) return "East";
-
         return to_dir[dir[idx]];
+        
     }
 };
 
