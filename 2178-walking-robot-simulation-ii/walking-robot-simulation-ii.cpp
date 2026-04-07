3ddc3896c165291a34cedeabf4ecd7e48c8cfa47
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-07 17:34:16 +0530

    Time: 24 ms (77.59%) | Memory: 128.3 MB (37.07%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -1,45 +1,33 @@
 class Robot {
 public:
-    bool moved=false;
-    int idx=0;
-    vector<int> dir;
-    vector<pair<int, int>> pos;
-    unordered_map<int, string> to_dir={
-        {0,"East"}, {1,"North"}, {2,"West"}, {3,"South"}
-    };
+    int W, H, P;
+    int pos=0;
+    bool hasMoved=false; 
     Robot(int width, int height) {
-        for(int i=0;i<width;i++){
-            pos.emplace_back(i,0);
-            dir.emplace_back(0);
-        }
-        for(int i=1;i<height;i++){
-            pos.emplace_back(width-1,i);
-            dir.emplace_back(1);
-        }
-        for(int i=width-2;i>=0;i--){
-            pos.emplace_back(i,height-1);
-            dir.emplace_back(2);
-        }
-        for(int i=height-2;i>0;i--){
-            pos.emplace_back(0,i);
-            dir.emplace_back(3);
-        }
-        dir[0]=3;
+        W=width-1;
+        H=height-1;
+        P=2*(W+H);
     }
     
     void step(int num) {
-        moved=true;
-        idx=(idx+num)%pos.size();
+        hasMoved=true;
+        pos=(pos+num)%P;
     }
     
     vector<int> getPos() {
-        return {pos[idx].first,pos[idx].second};
+        if(pos<=W) return {pos, 0};
+        if(pos<=W+H) return {W,pos-W};
+        if(pos<=2*W+H) return {W-(pos-(W+H)),H};
+        return {0,H-(pos-(2*W+H))};
     }
     
     string getDir() {
-        if(!moved) return "East";
-        return to_dir[dir[idx]];
-        
+        if(!hasMoved) return "East";
+        if(pos==0) return "South";
+        if(pos<=W) return "East";
+        if(pos<=W+H) return "North";
+        if(pos<=2*W+H) return "West";
+        return "South";
     }
 };
 
