03bdfb04b8f47239b96134e6040c663355f738b7
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-07 11:19:53 +0530

    Time: 28 ms (67.24%) | Memory: 127.3 MB (100.00%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.cpp
@@ -1,47 +1,49 @@
 class Robot {
 public:
-    int w,h;
-    int x=0,y=0;
-    int dir=0;
-    int per;
+    int w=0, h=0;
+    int x=0, y=0;
+    int dir=0, per=0;
     Robot(int width, int height) {
         w=width;
         h=height;
-        per=2*(w+h)-4;
+
+        per = 2*(h+w)-4;
     }
     
     void step(int num) {
-        if (per==0) return;
-        num%=per;
-        if(num==0 && (x!=0 || y!=0)) return;
-        if(num==0 && x==0 && y==0){
+        if(per==0) return;
+        num %= per;
+        if(num==0 && x!=0 && y!=0) return;
+        if(num==0 && x==0 && y==0) {
             dir=3;
             return;
         }
         while(num>0){
             if(dir==0){
-                int move=min(num, w-1-x);
+                int move=min(num,w-1-x);
                 x+=move;
                 num-=move;
                 if(num>0) dir=1;
             }
             else if(dir==1){
-                int move=min(num, h-1-y);
+                int move=min(num,h-1-y);
                 y+=move;
                 num-=move;
                 if(num>0) dir=2;
-            }else if(dir==2){
-                int move=min(num, x);
+            }
+            else if(dir==2){
+                int move=min(num,x);
                 x-=move;
                 num-=move;
                 if(num>0) dir=3;
-            }else {
-                int move=min(num, y);
+            }else{
+                int move=min(num,y);
                 y-=move;
                 num-=move;
                 if(num>0) dir=0;
             }
         }
+        
     }
     
     vector<int> getPos() {
