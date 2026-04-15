85faca247c2d9e539ae016469e1e1d0b729c7018
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-15 09:06:55 +0530

    Time: 31 ms (62.16%) | Memory: 13.6 MB (69.59%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     vector<vector<long long>> dp;
+    int n, m;
     long long solve(int i, int j, vector<int>& robot, vector<vector<int>>& factory){
-        int n=robot.size();
-        int m=factory.size();
 
         if(i==n) return 0;
         if(j==m) return 1e15;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        if(dp[i][j] !=-1) return dp[i][j];
+        long long res=solve(i,j+1,robot, factory);
 
-        long long res=solve(i, j+1, robot, factory);
-
-        long long cost =0;
+        long long cost=0;
         int pos=factory[j][0];
         int limit=factory[j][1];
 
         for(int k=0;k<limit && i+k<n;k++){
             cost += abs(robot[i+k]-pos);
-            res=min(res, cost+solve(i+k+1,j+1,robot,factory));
+            res=min(res,cost+solve(i+k+1, j+1, robot, factory));
         }
-
-        return dp[i][j] = res;
+        return dp[i][j]=res;
     }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+
         sort(robot.begin(), robot.end());
         sort(factory.begin(), factory.end());
+        n=robot.size();
+        m=factory.size();
+        dp.resize(n, vector<long long>(m,-1));
 
-        int n=robot.size();
-        int m=factory.size();
-
-        dp.assign(n, vector<long long>(m, -1));
-
-        return solve(0, 0, robot, factory);
+        return solve(0,0,robot,factory);
     }
 };
