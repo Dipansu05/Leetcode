841c2b2a7086cbf27ab34fcdf2d4b135051e85b2
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-20 16:56:19 +0530

    Time: 40 ms (63.20%) | Memory: 81.6 MB (91.87%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
-        return a[0] < b[0];
+    static bool comp(vector<int> &a, vector<int>&b){
+        if(a[0]==b[0]) return a[1]>b[1];
+        else return a[0]<b[0];
     }
-    int maxEnvelopes(vector<vector<int>>& env) {
-        int n = env.size();
-        sort(env.begin(), env.end(), cmp);
-        vector<int> lis;
-
-        for(int i=0;i<env.size();i++){
-            int ele = env[i][1];
-
-            int idx=lower_bound(lis.begin(), lis.end(), ele) - lis.begin();
-
-            if(idx >= lis.size()) lis.push_back(ele);
-            else lis[idx] = ele;
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), comp);
+        vector<int> temp;
+        temp.push_back(envelopes[0][1]);
+        for(int i=1;i<envelopes.size();i++){
+            if(temp.back()<envelopes[i][1]){
+                    temp.push_back(envelopes[i][1]);
+                }else{
+                    auto ind=lower_bound(temp.begin(), temp.end(), envelopes[i][1])-temp.begin();
+                    temp[ind]=envelopes[i][1];
+                }
+            }
+            return temp.size();
         }
-
-        return lis.size();
-    }
+    
 };
