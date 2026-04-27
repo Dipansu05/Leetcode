566514e715a18a16327346ee3c88cb903b0e43d5
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-27 12:43:28 +0530

    Time: 31 ms (38.71%) | Memory: 57.6 MB (59.68%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     bool hasValidPath(vector<vector<int>>& grid) {
         int m=grid.size(), n=grid[0].size();
-        vector<vector<pair<int, int>>> dir(7);
-        dir[1]={{0,-1},{0,1}};
-        dir[2]={{-1,0},{1,0}};
-        dir[3]={{0,-1},{1,0}};
-        dir[4]={{0,1},{1,0}};
-        dir[5]={{0,-1},{-1,0}};
-        dir[6]={{0,1},{-1,0}};
+
+        vector<vector<vector<int>>> dirs={
+            {},
+            {{0,-1},{0,1}},
+            {{-1,0},{1,0}},
+            {{0,-1},{1,0}},
+            {{0,1},{1,0}},
+            {{0,-1},{-1,0}},
+            {{0,1},{-1,0}}
+        };
 
         queue<pair<int,int>> q;
-        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<bool>> vis(m, vector<bool>(n,false));
 
         q.push({0,0});
-        vis[0][0]=1;
+        vis[0][0]=true;
 
         while(!q.empty()){
             auto [r,c]=q.front();
             q.pop();
 
             if(r==m-1 && c==n-1) return true;
-
-            for(auto[dr,dc] : dir[grid[r][c]]){
-                int nr=r+dr;
-                int nc=c+dc;
+            for(auto &d : dirs[grid[r][c]]){
+                int nr=r+d[0];
+                int nc=c+d[1];
 
                 if(nr<0 || nc<0 || nr>=m || nc>=n || vis[nr][nc]) continue;
 
-                for(auto[bdr,bdc] : dir[grid[nr][nc]]){
-                    if(nr+bdr==r && nc+bdc==c){
-                         vis[nr][nc]=1;
-                         q.push({nr,nc});
+                for(auto &back : dirs[grid[nr][nc]]){
+                    if(nr+back[0]==r && nc+back[1]==c){
+                        vis[nr][nc]=true;
+                        q.push({nr,nc});
                     }
                 }
             }
