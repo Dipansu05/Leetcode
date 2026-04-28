097ff0744c0e6855858ccb88ef79cdd0bcfd1cc7
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-28 09:25:00 +0530

    Time: 57 ms (10.29%) | Memory: 99.8 MB (5.35%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -3,25 +3,25 @@ public:
     int minOperations(vector<vector<int>>& grid, int x) {
         vector<int> arr;
 
-        for(auto &row : grid){
-            for(int v : row)
+        for(auto x: grid){
+            for(int v : x)
                 arr.push_back(v);
         }
 
         int base=arr[0];
         for(int v : arr){
-            if(abs(v-base)%x!=0) return -1;
+            if(abs(v-base) % x != 0) return -1;
         }
 
-        sort(arr.begin(), arr.end());
+        sort(begin(arr),end(arr));
 
         int median=arr[arr.size()/2];
+        int ops{0};
 
-        int ops=0;
         for(int v : arr){
-            ops+=abs(v-median)/x;
+           ops += abs(v-median)/x;       
         }
-        return ops;
 
+        return ops;
     }
 };
