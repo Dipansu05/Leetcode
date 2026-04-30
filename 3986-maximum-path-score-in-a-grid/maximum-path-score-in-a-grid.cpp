0508d9eddc7f20e693d88df2478e5e8cdd0fe3be
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-04-30 12:13:26 +0530

    Time: 904 ms (37.46%) | Memory: 499.4 MB (62.72%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<vector<int>>& grid, int k,  vector<vector<vector<int>>> &dp){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(k<0){
+            return -1e9;
+        }
+        if(i==m || j==n){
+            return -1e9;
+        }
+
+        if(i==m-1 && j==n-1){
+            if(min(grid[i][j],1)<=k) return grid[i][j];
+            else return -1e9;
+        }
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
+
+        int right=grid[i][j]+solve(i,j+1,grid,k-min(grid[i][j],1),dp);
+        int down=grid[i][j]+solve(i+1,j,grid,k-min(grid[i][j],1),dp);
+
+        return dp[i][j][k]=max(right,down);
+    }
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
-        const int NEG=-1e9;
 
-        vector<vector<int>> prev(n,vector<int>(k+1,NEG));
-        for(int i=0;i<m;i++){
-            vector<vector<int>> curr(n, vector<int>(k+1,NEG));
 
-            for(int j=0;j<n;j++){
-                int gain=grid[i][j];
-                int need=(gain>0?1:0);
 
-                int limit=min(k,i+j);
 
-                if(i==0 && j==0){
-                    curr[0][0]=0;
-                    continue;
-                }
 
-                for(int c=need;c<=limit;c++){
-                    int best=NEG;
-
-                    if(i>0 && prev[j][c-need] != NEG){
-                        best=max(best, prev[j][c-need]+gain);
-                    }
-
-                    if(j>0 && curr[j-1][c-need]!=NEG){
-                        best=max(best, curr[j-1][c-need]+gain);
-                    }
-
-                    curr[j][c]=best;
-                }
-            }
-
-            prev.swap(curr);
-        }
-
-        int ans=NEG;
-        for(int c=0;c<=k;c++){
-            ans=max(ans,prev[n-1][c]);
-        }
-
-        return ans<0?-1:ans;
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n, vector<int>(k+1,-1)));
+    
+        int ans=solve(0,0,grid,k,dp);
+        if(ans<0) return -1;
+        return ans;
     }
 };
