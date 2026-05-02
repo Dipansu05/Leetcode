90b7dc171ce191010ded1a1cb05e5890fd6db36a
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-02 18:52:29 +0530

    Time: 8 ms (34.25%) | Memory: 20.2 MB (24.39%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int>indgr(n,0);
-        vector<int>v[n];
-        for(auto it:pre){
-            v[it[1]].push_back(it[0]);
-        }
+    bool topoSort(unordered_map<int, vector<int>>& adj, int n, vector<int>& indegree){
+        queue<int> q;
+        int count{0};
         for(int i=0;i<n;i++){
-        for(auto it:v[i]){
-            indgr[it]++;
-        }
-        }
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(indgr[i]==0){
+            if(indegree[i]==0){
+                count++;
                 q.push(i);
             }
         }
-        int cnt=0;
+
         while(!q.empty()){
-            int node=q.front();
-            q.pop();cnt++;
-            for(auto it:v[node]){
-                indgr[it]--;
-                if(indgr[it]==0){
-                    q.push(it);
+            int u=q.front();
+            q.pop();
+
+            for(int &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    count++;
+                    q.push(v);
                 }
             }
+
+
         }
-        if(cnt==n) return 1;
-        else return 0;
+        if(count==n) return true;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(n,0);
+
+        for(auto &vec : prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+
+        return topoSort(adj,n,indegree);
     }
 };
