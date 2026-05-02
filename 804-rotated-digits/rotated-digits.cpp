2da93400ec77a6df150d7e6ec935c99b75193434
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-02 12:33:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (17.02%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<=n;i++){
             if(i<10){
                 if(i==0 || i==1 || i==8) dp[i]=1;
-                else if(i==2 || i==5 || i==6 ||i==9){
+                else if(i==2 || i==5 || i ==6 || i==9){
                     dp[i]=2;
                     count++;
                 }
@@ -17,9 +17,9 @@ public:
                 int b=dp[i%10];
 
                 if(a==1 && b==1) dp[i]=1;
-                else if(a>=1 && b>=1){
+                else if(a>=1 && b>=1) {
                     dp[i]=2;
-                    count ++;
+                    count++;
                 }
                 else dp[i]=0;
             }
