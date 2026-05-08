f26f3535fbaf7152c5f0f29ffc2df75bfadbf945
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-08 13:31:10 +0530

    Time: 1222 ms (25.09%) | Memory: 454.5 MB (7.75%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -1,97 +1,81 @@
 class Solution {
 public:
-    static constexpr int MAXV=1'000'001;
-    static const vector<int>& getSPF(){
-        static const vector<int> spf=[](){
-            vector<int> s(MAXV);
-            for(int i=0;i<MAXV;i++){
-                s[i]=i;
-            }
-
-            s[0]=0;
-            s[1]=1;
-            for(long long i=2; i*i < MAXV; i++){
-                if(s[i]==i){
-                    for(long long j=i*i;j<MAXV; j+=i){
-                        if(s[j]==j) s[j]=i;
-                    }
-                }
-            }
-            return s;
-        }();
-        return spf;
-    }
-    bool isPrime(int x, const vector<int>& spf){
-        return x>=2 && spf[x]==x;
-    }
     int minJumps(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return 0;
-        const auto& spf=getSPF();
+        int mx=*max_element(nums.begin(), nums.end());
+        vector<int> spf(mx+1);
 
-        int maxi=*max_element(nums.begin(), nums.end());
-        vector<char> primeSeen(maxi+1, false);
-
-        for(int x : nums){
-            if(isPrime(x, spf)){
-                primeSeen[x]=true;
-            }
+        for(int i=0;i<=mx;i++){
+            spf[i]=i;
         }
 
+        for(int i=2;i*i<=mx;i++){
+            if(spf[i]==i){
+                for(int j=i*i;j<=mx;j+=i){
+                    if(spf[j]==j) spf[j]=i;
+                }
+            }
+        }
         unordered_map<int, vector<int>> mp;
-        mp.reserve(n*2);
 
         for(int i=0;i<n;i++){
             int x=nums[i];
-            if(x==1) continue;
+
+            unordered_set<int> used;
             while(x>1){
                 int p=spf[x];
-                if(p<=maxi && primeSeen[p]){
-                mp[p].push_back(i);
+
+                if(!used.count(p)){
+                    mp[p].push_back(i);
+                    used.insert(p);
                 }
 
-                while(x%p==0){
-                    x/=p;
-                }
+                x/=p;
             }
         }
-        vector<int> dist(n, -1);
-        vector<char> usedPrime(maxi+1, false);
+
         queue<int> q;
+        vector<int> dist(n,-1);
         q.push(0);
         dist[0]=0;
 
         while(!q.empty()){
-            int idx = q.front();
+            int i=q.front();
             q.pop();
 
-            if(idx==n-1) return dist[idx];
+            int steps=dist[i];
+            if(i==n-1) return steps;
 
-            if(idx-1 >=0 && dist[idx-1]==-1){
-                dist[idx-1]=dist[idx]+1;
-                q.push(idx-1);
+            if(i-1>=0 && dist[i-1]==-1){
+                dist[i-1]=steps+1;
+                q.push(i-1);
             }
 
-            if(idx+1<n && dist[idx+1]==-1){
-                dist[idx+1]=dist[idx]+1;
-                q.push(idx+1);
+            if(i-1>=0 && dist[i-1]==-1){
+                dist[i-1]=steps+1;
+                q.push(i-1);
             }
 
-            int x=nums[idx];
+            if(i+1<n && dist[i+1]==-1){
+                dist[i+1]=steps+1;
+                q.push(i+1);
+            }
 
-            if(x<=maxi && isPrime(x, spf) && !usedPrime[x]){
-                usedPrime[x]=true;
-                auto it = mp.find(x);
-                if(it != mp.end()){
-                    for(int nextIdx : it->second){
-                        if(dist[nextIdx]==-1){
-                            dist[nextIdx]=dist[idx]+1;
-                            q.push(nextIdx);
-                        }
+            int val=nums[i];
+
+            if(val > 1 && spf[val]==val){
+                for(int nxt : mp[val]){
+                    if(dist[nxt]==-1){
+                        dist[nxt]=steps+1;
+                        q.push(nxt);
                     }
                 }
+
+                mp[val].clear();
             }
         }
+
         return -1;
     }
 };
