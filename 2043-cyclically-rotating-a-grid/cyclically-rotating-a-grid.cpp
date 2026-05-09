42c658407ae8082162ff0d104053fd16029df10a
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-09 10:33:49 +0530

    Time: 21 ms (11.76%) | Memory: 17.8 MB (52.21%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -3,27 +3,25 @@ public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
         int m=grid.size();
         int n=grid[0].size();
-
-        int layers=min(m,n)/2;
+        int layers=min(m,n) /2;
 
         for(int layer=0;layer<layers;layer++){
-            vector<int> vals;
             int top=layer;
             int left=layer;
-
             int bottom=m-layer-1;
             int right=n-layer-1;
+            vector<int> vals;
 
-            for(int j=left;j<=right;j++){
-                vals.push_back(grid[top][j]);
+            for(int i=left;i<=right;i++){
+                vals.push_back(grid[top][i]);
             }
 
             for(int i=top+1;i<=bottom-1;i++){
                 vals.push_back(grid[i][right]);
             }
 
-            for(int j=right;j>=left;j--){
-                vals.push_back(grid[bottom][j]);
+            for(int i=right;i>=left;i--){
+                vals.push_back(grid[bottom][i]);
             }
 
             for(int i=bottom-1;i>=top+1;i--){
@@ -37,25 +35,25 @@ public:
             for(int j=left;j<=right;j++){
                 grid[top][j]=vals[idx];
                 idx++;
-                if(idx==sz) idx=0;
+                if(idx==sz) idx=0; 
             }
 
-            for(int i=top+1;i<=bottom-1;i++){
-                grid[i][right]=vals[idx];
+            for(int j=top+1;j<=bottom-1;j++){
+                grid[j][right]=vals[idx];
                 idx++;
-                if(idx==sz) idx=0;
+                if(idx==sz) idx=0; 
             }
 
             for(int j=right;j>=left;j--){
                 grid[bottom][j]=vals[idx];
                 idx++;
-                if(idx==sz) idx=0;
+                if(idx==sz) idx=0; 
             }
 
-            for(int i=bottom-1;i>=top+1;i--){
-                grid[i][left]=vals[idx];
+            for(int j=bottom-1;j>=top+1;j--){
+                grid[j][left]=vals[idx];
                 idx++;
-                if(idx==sz) idx=0;
+                if(idx==sz) idx=0; 
             }
         }
 
