191f44ec2ef5f8f6f0000663c19df149055ff74c
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-11 17:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (49.70%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> parent;
     vector<int> rank;
-
     int find(int x){
-        if(x==parent[x]) return x;
+        if(parent[x]==x) return x;
+
         return parent[x]=find(parent[x]);
     }
-
     void Union(int x, int y){
         int x_parent=find(x);
         int y_parent=find(y);
-
-        if(x_parent==y_parent) return;
-
-        if(rank[x_parent]>rank[y_parent]) parent[y_parent]=x_parent;
-        else if(rank[x_parent]<rank[y_parent]) parent[x_parent]=y_parent;
-        else{
+        if(x_parent == y_parent) return;
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }else{
             parent[x_parent]=y_parent;
             rank[y_parent]++;
         }
@@ -25,9 +24,7 @@ public:
         parent.resize(26);
         rank.resize(26, 0);
 
-        for(int i=0;i<26;i++){
-            parent[i]=i;
-        }
+        for(int i=0;i<26;i++) parent[i]=i;
 
         for(string &s : equations){
             if(s[1]=='='){
@@ -39,10 +36,9 @@ public:
             if(s[1]=='!'){
                 char first=s[0];
                 char second=s[3];
-
-                int parent_first=find(first-'a');
-                int parent_second=find(second-'a');
-                if(parent_first==parent_second) return false;
+                int first_parent=find(first-'a');
+                int second_parent=find(second-'a');
+                if(first_parent==second_parent) return false;
             }
         }
         return true;
