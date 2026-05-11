189868c15dcad9d72db3a0e129568f37af823c81
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-11 18:50:47 +0530

    Time: 3 ms (85.09%) | Memory: 42.2 MB (72.62%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
-    void dfs_graph(vector<vector<int>> &graph, int u, vector<bool> &vis){
-        vis[u]=true;
-        for(auto &v: graph[u]){
-            if(!vis[v]) dfs_graph(graph,v,vis);
+    vector<int> rank;
+    vector<int> parent;
+    int find(int x){
+        if(x==parent[x]) return x;
+
+        return parent[x]=find(parent[x]);
+    }
+
+    void Union(int x, int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent) return;
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }else if(rank[y_parent]>rank[x_parent]){
+            parent[x_parent]=y_parent;
+        }else{
+            parent[x_parent]=y_parent;
+            rank[y_parent]++;
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<n-1) return -1;
+        parent.resize(n);
+        rank.resize(n, 0);
 
-        vector<vector<int>> graph(n);
-        vector<bool> vis(n, false);
+        for(int i=0;i<n;i++) parent[i]=i;
 
-        for(int i=0;i<connections.size();i++){
-            int a=connections[i][0];
-            int b=connections[i][1];
+        int components{n};
 
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-        }
-
-        int components{0};
-        for(int u=0;u<n;u++){
-            if(!vis[u]){
-                dfs_graph(graph,u,vis);
-                components++;
+        for(auto &vec : connections){
+            if(find(vec[0])!=find(vec[1])){
+                Union(vec[0], vec[1]);
+                components--;
             }
         }
         return components-1;
