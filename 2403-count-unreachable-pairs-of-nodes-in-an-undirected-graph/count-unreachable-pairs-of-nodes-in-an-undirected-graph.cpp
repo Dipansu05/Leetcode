1e2f0d6cdb9e95fffd3caa859a53a6bc077ebb89
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-13 11:19:22 +0530

    Time: 167 ms (31.33%) | Memory: 205.9 MB (20.49%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,50 +1,31 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
-    int find(int x){
-        if(parent[x]==x) return x;
-        return parent[x]=find(parent[x]);
-    }
-    void unite(int x, int y){
-        int x_parent=find(x);
-        int y_parent=find(y);
-
-        if(x_parent==y_parent) return;
-        if(rank[x_parent]>rank[y_parent]){
-            parent[y_parent]=x_parent;
-        }else if(rank[x_parent]<rank[y_parent]){
-            parent[x_parent]=y_parent;
-        }else{
-            parent[x_parent]=y_parent;
-            rank[y_parent]++;
+    void dfs(unordered_map<int, vector<int>> &mp, int u, vector<bool> &vis, int &Size){
+        vis[u]=true;
+        Size++;
+        for(auto &x : mp[u]){
+            if(!vis[x]) dfs(mp,x,vis,Size);
         }
+
     }
     long long countPairs(int n, vector<vector<int>>& edges) {
-        parent.resize(n);
-        rank.resize(n, 0);
-
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-
+        vector<bool> vis(n, false);
+        unordered_map<int, vector<int>> mp;
         for(auto &vec: edges){
-            int u=vec[0];
-            int v=vec[1];
-            unite(u, v);
+            mp[vec[0]].push_back(vec[1]);
+            mp[vec[1]].push_back(vec[0]);
         }
 
-        unordered_map<int, int> mp;
-        for(int i=0;i<n;i++){
-            int rep=find(i);
-            mp[rep]++;
-        }
         long long ans{0};
-        long long rem=n;
-        for(auto &it : mp){
-            int size=it.second;
-            ans+=size*(rem-size);
-            rem-=size;
+        long long rem{n};
+
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                int Size{0};
+                dfs(mp,i,vis,Size);
+                ans+=Size*(rem-Size);
+                rem-=Size;
+            }
         }
         return ans;
     }
