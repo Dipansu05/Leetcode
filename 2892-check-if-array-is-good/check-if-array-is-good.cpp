8aadcb1ddfcfa015820c942e050020f06708f67d
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-14 11:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (23.51%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -3,9 +3,9 @@ public:
     bool isGood(vector<int>& nums) {
         int n=nums.size();
         vector<int> freq(n,0);
-        for(int num : nums){
-            if(num<1 || num>=n) return false;
-            freq[num]++;
+        for(auto x: nums){
+            if(x<1 || x>=n) return false;
+            freq[x]++;
         }
         for(int i=1;i<n-1;i++){
             if(freq[i]!=1) return false;
