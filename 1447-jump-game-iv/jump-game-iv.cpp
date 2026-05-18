960105264eee6f19082644d7f0c2eede451d8f9c
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-18 12:12:56 +0530

    Time: 67 ms (94.86%) | Memory: 75 MB (84.58%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -2,40 +2,39 @@ class Solution {
 public:
     int minJumps(vector<int>& arr) {
         int n=arr.size();
+        vector<bool> vis(n, false);
         unordered_map<int, vector<int>> mp;
         for(int i=0;i<n;i++){
             mp[arr[i]].push_back(i);
         }
         queue<int> q;
-        vector<bool> vis(n, false);
         q.push(0);
         vis[0]=true;
         int ans{0};
-
-        for(;!q.empty();ans++){
-            int sz=q.size();
-            for(int i=0;i<sz;i++){
-                int index=q.front();
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                int i=q.front();
                 q.pop();
-                if(index==n-1) return ans;
-
-                for(int neigh: {index-1, index+1}){
-                    if(neigh>=0 && neigh<n && !vis[neigh]){
-                        vis[neigh]=true;
-                        q.push(neigh);
-                    }
-                    if(auto it=mp.find(arr[index]); it!=mp.end() && !it->second.empty()){
-                        for(const int neigh: it->second){
-                            if(!vis[neigh]){
-                                vis[neigh]=true;
-                                q.push(neigh);
-                            }
-                        }
-                        it->second.clear();
+                if(i==n-1) return ans;
+                if(i+1<n && !vis[i+1]){
+                    q.push(i+1);
+                    vis[i+1]=true;
+                }
+                if(i-1>=0 && !vis[i-1]){
+                    q.push(i-1);
+                    vis[i-1]=true;
+                }
+                for(int j: mp[arr[i]]){
+                    if(!vis[j]){
+                        q.push(j);
+                        vis[j]=true;
                     }
                 }
+            mp[arr[i]].clear();
             }
+            ans++;
         }
-        return ans;
+        return -1;
     }
 };
