13ed56c77d4484483d21bc6b1ffdfe4b316a9c17
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-19 12:16:08 +0530

    Time: 86 ms (47.68%) | Memory: 44.2 MB (81.39%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         unordered_map<int, vector<pair<int, int>>> adj;
-        for(auto &vec: times){
+        for(auto &vec : times){
             int u=vec[0];
             int v=vec[1];
             int w=vec[2];
-            adj[u].push_back({v,w});
+            adj[u].push_back({v, w});
         }
-        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;  
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         vector<int> result(n+1, INT_MAX);
         result[k]=0;
-        pq.push({0,k});
-
+        pq.push({0, k});
         while(!pq.empty()){
             int d=pq.top().first;
             int node=pq.top().second;
@@ -31,6 +31,6 @@ public:
         for(int i=1;i<=n;i++){
             ans=max(ans, result[i]);
         }
-        return ans==INT_MAX ? -1 : ans;
+        return ans==INT_MAX? -1: ans;
     }
 };
