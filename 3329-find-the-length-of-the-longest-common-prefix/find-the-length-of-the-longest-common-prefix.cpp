76b51ea29aeb25e9f674c310ad321e2c061e29eb
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-21 09:31:38 +0530

    Time: 135 ms (79.17%) | Memory: 134.6 MB (98.00%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -10,23 +10,21 @@ public:
     }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
         unordered_set<int> pref;
-
+        int ans{0};
         for(int num: arr1){
             int x=num;
             while(x>0){
                 pref.insert(x);
                 x/=10;
-            }
+                }
         }
-        int ans{0};
 
         for(int num: arr2){
             int x=num;
             int len=digits(num);
             while(x>0){
                 if(pref.count(x)){
-                    ans=max(ans,len);
-                    break;
+                    ans=max(ans, len);
                 }
                 x/=10;
                 len--;
