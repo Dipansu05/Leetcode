022167bb2dbbd69b43ca7d7a63a4d91b1d305efe
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-25 12:33:57 +0530

    Time: 12 ms (58.16%) | Memory: 19 MB (55.39%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int n;
-    int dfs(int i, vector<int>& arr, int d, vector<int>& dp){
+    int dfs(vector<int>& arr, int i, vector<int>& dp, int d){
         if(dp[i]!=-1) return dp[i];
-
         int ans{1};
-        for(int j=i+1;j<=min(n-1,i+d);j++){
+
+        for(int j=i+1;j<=min(n-1, i+d);j++){
             if(arr[j]>=arr[i]) break;
-            ans=max(ans,1+dfs(j,arr,d,dp));
+            ans=max(ans, 1+dfs(arr, j, dp, d));
         }
 
-        for(int j=i-1;j>=max(0,i-d);j--){
+        for(int j=i-1;j>=max(0, i-d);j--){
             if(arr[j]>=arr[i]) break;
-            ans=max(ans,1+dfs(j,arr,d,dp));
+            ans=max(ans, 1+dfs(arr, j, dp, d));
         }
+
         return dp[i]=ans;
     }
     int maxJumps(vector<int>& arr, int d) {
-        n = arr.size();
-        vector<int> dp(n , -1);
+        n=arr.size();
         int ans{1};
+        vector<int> dp(n, -1);
         for(int i=0;i<n;i++){
-            ans = max(ans, dfs(i, arr, d, dp));
-        }   
-        return ans;
+            ans=max(ans, dfs(arr, i, dp, d));
+        }
+        return ans; 
     }
 };
