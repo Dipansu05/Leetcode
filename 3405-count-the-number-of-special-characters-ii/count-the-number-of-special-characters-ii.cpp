4f88f3bca92ede6b2d71aa154f4a86a7c9c38ccf
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-27 09:50:29 +0530

    Time: 69 ms (63.48%) | Memory: 24.4 MB (68.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -3,19 +3,22 @@ public:
     int numberOfSpecialChars(string word) {
         vector<int> firstUpper(26, -1);
         vector<int> lastLower(26, -1);
+        int count{0};
 
         for(int i=0;i<word.size();i++){
-            char c = word[i];
-            if(islower(c)) lastLower[c-'a']=i;
-            else if(isupper(c)){
-                int idx=c-'A';
-                if(firstUpper[idx]==-1) firstUpper[idx]=i;
+            char ch=word[i];
+            if(islower(ch)) lastLower[ch-'a']=i;
+            else if(isupper(ch)){
+                if(firstUpper[ch-'A']==-1) firstUpper[ch-'A']=i;
             }
         }
-        int count{0};
+
         for(int i=0;i<26;i++){
-            if(lastLower[i] != -1 && firstUpper[i] !=-1 && lastLower[i]<firstUpper[i]) count ++;
+            if(lastLower[i] != -1 && firstUpper[i] != -1 && lastLower[i] < firstUpper[i]){
+                count ++;
+            }
         }
+
         return count;
     }
 };
