ed4ac14b211703cf507b09628c4abbbb051cf269
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-05-29 19:36:41 +0530

    Time: 140 ms (67.80%) | Memory: 160.8 MB (32.83%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,35 +1,63 @@
 class Solution {
 public:
-    int digits(int x){
-        int count{0};
-        while(x){
-            count++;
-            x /= 10;
+    struct trieNode{
+        trieNode* children[10];
+    };
+
+    trieNode* getNode(){
+        trieNode* newNode = new trieNode();
+
+        for(int i=0;i<10;i++){
+            newNode->children[i] = nullptr;
         }
-        return count;
+
+        return newNode;
+
+    }
+
+    void insert(int num, trieNode* root){
+        trieNode* crawl = root;
+        string numStr = to_string(num);
+
+        for(char ch: numStr){
+            int idx = ch - '0';
+            if(!crawl -> children[idx]){
+                crawl -> children[idx] = getNode();
+            }
+            crawl = crawl -> children[idx];
+        }
+    }
+
+    int search (int num, trieNode* root){
+        trieNode* crawl = root;
+        string numStr = to_string(num);
+        int length{0};
+
+        for(char ch: numStr){
+            int idx = ch-'0';
+            if(crawl -> children[idx]){
+                length++;
+                crawl = crawl -> children[idx];
+            }else{
+                break;
+            }
+        }
+        return length;
     }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st;
-        int ans{0};
+
+        trieNode* root = getNode();
+
         for(int num: arr1){
-            while(num){
-                st.insert(num);
-                num /= 10;
-            }
+            insert(num, root);
         }
 
+        int result{0};
         for(int num: arr2){
-            int x = num;
-            int len = digits(x);
-            while(x){
-                if(st.count(x)){
-                    ans=max(ans,len);
-                }
-                x /= 10;
-                len--;
-            }
+            result = max(result, search(num, root));
         }
 
-        return ans;
+        return result;
+        
     }
 };
