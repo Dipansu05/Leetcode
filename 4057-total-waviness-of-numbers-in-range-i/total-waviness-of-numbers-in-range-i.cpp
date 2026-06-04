447ad5a425e8a2e25914500eda19aadfb5a98c85
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-06-04 11:38:27 +0530

    Time: 54 ms (37.13%) | Memory: 10.3 MB (24.07%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-int waviness(int x){
-    string s = to_string(x);
-    int cnt{0};
-    for(int i=1;i<(int)s.size()-1;i++){
-        if(( s[i] > s[i-1] && s[i]> s [i+1] ) ||
-           ( s[i] < s[i-1] && s[i] < s[i+1] ))
-          cnt++;
+    int do_cal(int x){
+        string s = to_string(x);
+        int cnt{0};
+        for(int i=1;i<s.size()-1;i++){
+            if ((s[i]<s[i+1] && s[i]<s[i-1]) ||
+                (s[i]>s[i+1] && s[i]>s[i-1]))
+               cnt++;
+        }
+        return cnt;
     }
-    return cnt;
-}
     int totalWaviness(int num1, int num2) {
-        long long ans{0};
+        int ans{0};
         for(int x=num1;x<=num2;x++){
-            ans += waviness(x);
-        }   
+            ans += do_cal(x);
+        }
         return ans;
     }
 };
