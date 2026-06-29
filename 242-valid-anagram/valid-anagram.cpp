05b89ec52702ff173c3121240f77865a6074fa52
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-06-29 17:56:22 +0530

    Time: 3 ms (46.33%) | Memory: 9.9 MB (11.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return t==s;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char, int> sm;
+        unordered_map<char, int> tm;
+        for(int i=0;i<s.size();i++){
+            sm[s[i]]++;
+            tm[t[i]]++;
+        }
+        for(char x: s){
+            if (sm[x]!=tm[x]) return false;
+        }
+        return true;
+        
     }
 };
