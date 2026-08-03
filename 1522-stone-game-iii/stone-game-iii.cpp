9b75324d7da2951072e7cd93f97eb36c7298ee9b
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-08-03 12:03:30 +0530

    Time: 8 ms (94.81%) | Memory: 136 MB (83.46%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int n;
-    vector<int> t;
-    int solve(vector<int>& stones, int i){
-        if(i>=n) return 0;
-        if(t[i]!=-1) return t[i];
-        int res = stones[i] - solve(stones, i+1);
-        if(i+1<n)
-            res =  max(res, stones[i]+stones[i+1]-solve(stones, i+2));
-        if(i+2<n)
-            res = max(res, stones[i]+stones[i+2]+stones[i+1]-solve(stones, i+3));
-
-        return t[i]=res;
-    }
+    
     string stoneGameIII(vector<int>& stones) {
         n = stones.size();
-        t.resize(n+1, -1);
-        int res = solve(stones, 0);
+        //t.resize(n+1, -1);
+        vector<int> t(n+1);
+        for(int i=n-1;i>=0;i--){
+            t[i]=stones[i]-t[i+1];
+            if(i+2<=n)
+                t[i]=max(t[i], stones[i]+stones[i+1]-t[i+2]);
+            if(i+3<=n)
+                t[i]=max(t[i], stones[i]+stones[i+1]+stones[i+2]-t[i+3]);
+        }
+        int res = t[0];
         if(res > 0) return "Alice";
         if(res < 0) return "Bob";
         return "Tie";
