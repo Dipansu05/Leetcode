804617af3414e6be9851db6fb18eb138b71ed563
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-08-03 11:04:55 +0530

    Time: 41 ms (39.39%) | Memory: 142.4 MB (31.45%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& stone) {
-        int n = stone.size();
-        vector<int> dp(n+1, 0);
-        for(int i=n-1;i>=0;i--){
-            dp[i]=INT_MIN;
-            int sum{0};
-            for(int j=i;j<min(n,i+3);j++){
-                sum += stone[j];
-                dp[i] = max(dp[i], sum-dp[j+1]);
-            }
-        }
-        if(dp[0]>0) return "Alice";
-        if(dp[0]<0) return "Bob";
+    int n;
+    vector<int> t;
+    int solve(vector<int>& stones, int i){
+        if(i>=n) return 0;
+        if(t[i]!=-1) return t[i];
+        int res = stones[i] - solve(stones, i+1);
+        if(i+1<n)
+            res =  max(res, stones[i]+stones[i+1]-solve(stones, i+2));
+        if(i+2<n)
+            res = max(res, stones[i]+stones[i+2]+stones[i+1]-solve(stones, i+3));
+
+        return t[i]=res;
+    }
+    string stoneGameIII(vector<int>& stones) {
+        n = stones.size();
+        t.resize(n+1, -1);
+        int res = solve(stones, 0);
+        if(res > 0) return "Alice";
+        if(res < 0) return "Bob";
         return "Tie";
     }
 };
