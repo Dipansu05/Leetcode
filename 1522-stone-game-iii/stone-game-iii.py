e0500713a529c9f7657d98b9393410762659e57f
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-08-03 12:12:01 +0530

    Time: 406 ms (94.81%) | Memory: 24 MB (71.59%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -1,32 +1,20 @@
 class Solution:
     def stoneGameIII(self, stones: List[int]) -> str:
         n = len(stones)
-        dp = [-1] * (n+1)
+        t = [0] * (n+1)
 
-        def solve(i):
-            if i>=n:
-                return 0
+        for i in range(n-1,-1, -1):
+            t[i] = stones[i] - t[i+1]
+            if i + 2 <= n:
+                t[i] = max(t[i], stones[i]+stones[i+1]-t[i+2])
+            if i + 3 <= n:
+                t[i] = max(t[i], stones[i]+stones[i+1]+stones[i+2]-t[i+3])
 
-            if dp[i] != -1:
-                return dp[i]
-
-            res = stones[i] - solve(i+1)
-
-            if i+1<n:
-                res = max(res, stones[i]+stones[i+1]-solve(i+2))
-
-            if i+2<n:
-                res = max(res, stones[i]+stones[i+1]+stones[i+2]-solve(i+3))
-
-            dp[i] = res
-            return res
-
-        ans = solve(0)
-        if ans > 0:
+        res = t[0]
+        if res > 0:
             return "Alice"
-        elif ans < 0:
+        elif res<0:
             return "Bob"
         else:
             return "Tie"
-
-
+        
