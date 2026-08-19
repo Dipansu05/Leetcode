255cf2a9df6db501303cae0db77a02c814bb856c
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-08-19 19:12:42 +0530

    Time: 16 ms (85.98%) | Memory: 59.2 MB (78.38%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        int ans = n*2;
-        sort(reservedSeats.begin(),reservedSeats.end());
-        for(int i=0;i<reservedSeats.size();i++){
-            int curr=reservedSeats[i][0];
-            int flag25=1;
-            int flag47=1;
-            int flag69=1;
+        unordered_map<int, int> rows;
+        for(const auto& seat : reservedSeats){
+            int row = seat[0];
+            int col = seat[1];
 
-            while(i<reservedSeats.size() && reservedSeats[i][0]==curr){
-                if(reservedSeats[i][1]>=2 && reservedSeats[i][1]<=5){
-                    flag25=0;
-                    if(reservedSeats[i][1]>=4 && reservedSeats[i][1]<=5){
-                        flag47 = 0;
-                        }
-                    }else if(reservedSeats[i][1]>=6 && reservedSeats[i][1]<=9){
-                        flag69=0;
-                        if(reservedSeats[i][1]>=6 && reservedSeats[i][1]<=7){
-                            flag47=0;
-                        }
-                    }
-                    i++;
-                }
-                if(flag69==0 && flag25==0 && flag47==0){
-                    ans--;
-                    ans--;
-                }else if(flag69==0 || flag47==0 || flag25==0){
-                    ans--;
-                }
-                i--;
-            
+            if(col>=2 && col<=9){
+                rows[row] |= (1<<col);
+            }
         }
-        return ans;
+
+        int answer = 2 * (n-static_cast<int> (rows.size()));
+
+        const int left = (1<<2) | (1<<3) | (1<<4) | (1<<5);
+        const int middle = (1<<4) | (1<<5) | (1<<6) | (1<<7);
+        const int right = (1<<6) | (1<<7) | (1<<8) | (1<<9);
+
+        for(const auto& [row, mask] : rows){
+            bool canLeft = (mask & left) == 0;
+            bool canMiddle = (mask & middle) == 0;
+            bool canRight = (mask & right) == 0;
+            if(canLeft && canRight) answer += 2;
+            else if (canLeft || canMiddle || canRight) answer += 1;
+        }
+
+        return answer;
     }
 };
