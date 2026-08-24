b57e94eb5903420b54c3ce64860b0959d22952b1
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-08-24 20:15:53 +0530

    Time: 114 ms (35.29%) | Memory: 95.7 MB (40.85%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.cpp
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
+    int n;
+    
+    int t[100001];
+
     int stoneGameVIII(vector<int>& stones) {
-        for(int i=1;i<stones.size();i++){
-            stones[i] += stones[i-1];
-        }
-        int best = stones.back();
-        for(int i=stones.size()-2;i>=1;i--){
-            best = max(best, stones[i]-best);
-        }
-        return best;
+     memset(t, -1, sizeof(t));
+     n=stones.size();
+     vector<int> pref(n, 0);
+     pref[0]=stones[0];
+     for(int i=1;i<n;i++){
+        pref[i]=stones[i]+pref[i-1];
+     }
+     vector<int> t(n,0);
+     t[n-1]=pref[n-1];
+     for(int i=n-2;i>=1;i--){
+        int take=pref[i]-t[i+1];
+        int skip=t[i+1];
+        t[i] = max(take, skip);
+     }
+     return t[1];
     }
 };
