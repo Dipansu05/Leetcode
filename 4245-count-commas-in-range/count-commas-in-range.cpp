d7bc7077c2f2a73bfbf0a7dcb8512504d2236897
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-09-08 14:29:31 +0530

    Time: 2 ms (15.77%) | Memory: 8.4 MB (99.47%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        long long answer = 0;
-        long long threshold = 1000;
-        while(threshold<=n){
-            answer += n-threshold+1;
-            threshold *= 1000;
-        }
-        return static_cast<int>(answer);
+        return n < 1000 ? 0 : n - 999;   
     }
 };
