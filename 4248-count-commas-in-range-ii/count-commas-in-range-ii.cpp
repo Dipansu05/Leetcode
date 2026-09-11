f720ce05e9421f95f1b2d5a19527724d33b02a67
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-09-11 10:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (99.04%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -3,9 +3,15 @@ public:
     using ll = long long;
     long long countCommas(long long n) {
         ll ans = 0;
-        for(ll t=1000, d=1;t<=n;t*=1000,d++){
-            ll x = min(n+1, 1000*t);
-            ans += (x-t)* d;
+        ll commas = 1;
+        ll lower = 1000;
+        while ( lower <= n ){
+            ll upper = 1000 * lower - 1;
+            if (upper > n) upper = n;
+            ll nums_comm = upper - lower + 1;
+            ans += (commas * nums_comm);
+            commas ++;
+            lower *= 1000;
         }
         return ans;
     }
