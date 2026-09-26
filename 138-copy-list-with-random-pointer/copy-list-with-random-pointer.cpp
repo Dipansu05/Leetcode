391d8446896307607dfd8357b0fd67ff85d663fe
Dipansu05 <115099812+Dipansu05@users.noreply.github.com>
2026-09-26 22:47:07 +0530

    Time: 8 ms (27.79%) | Memory: 15.1 MB (51.11%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -18,18 +18,22 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         unordered_map<Node*, Node*> oldToCopy;
-        oldToCopy[NULL] = NULL;
+        oldToCopy[nullptr] = nullptr;
+
         Node* cur = head;
-        while(cur != NULL){
-            Node* copy = new Node(cur->val);
-            oldToCopy[cur] = copy;
-            cur = cur->next;
-        }
-        cur = head;
-        while(cur!=NULL){
-            Node* copy = oldToCopy[cur];
-            copy->next = oldToCopy[cur->next];
-            copy->random = oldToCopy[cur->random];
+        while(cur != nullptr){
+            if(oldToCopy.find(cur)==oldToCopy.end()){
+                oldToCopy[cur] = new Node(0);
+            }
+            oldToCopy[cur]->val = cur->val;
+            if(oldToCopy.find(cur->next)==oldToCopy.end()){
+                oldToCopy[cur->next] = new Node(0);
+            }
+            oldToCopy[cur]->next = oldToCopy[cur->next];
+            if(oldToCopy.find(cur->random)==oldToCopy.end()){
+                oldToCopy[cur->random] = new Node(0);
+            }
+            oldToCopy[cur]->random = oldToCopy[cur->random];
             cur = cur->next;
         }
         return oldToCopy[head];
